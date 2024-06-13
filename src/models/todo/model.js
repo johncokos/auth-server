e3260a83ed647db617f38fc97c5d8ea37de810d6
John Cokos <highlander.44@gmail.com>
2024-06-13 09:28:30 -0700

    fixed johns bug

--- src/models/todo/model.js
+++ src/models/todo/model.js
@@ -3,7 +3,7 @@
 const todoModel = (sequelize, DataTypes) => sequelize.define('Todo', {
   task: { type: DataTypes.STRING, required: true },
   assignee: { type: DataTypes.STRING, required: true },
-  difficulty: { type: DataTypes.NUMBER, required: true },
+  difficulty: { type: DataTypes.INTEGER, required: true },
   complete: { type: DataTypes.BOOLEAN, required: true, defaultValue: false}
 });
 
